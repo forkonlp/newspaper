42f60b9621b3a47a01602b77de8c150e9f20515a
Chanyub Park <mrchypark@gmail.com>
2019-06-30 14:02:52 +0900

    remove zealot add httr

---
+++ R/zdnet_co_kr.R
@@ -0,0 +1,6 @@
+read_zdnet_co_kr <- function(x) {
+  ah <- httr::add_headers("User-Agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36")
+  httr::GET(x, ah) %>%
+    httr::content()
+}
+
